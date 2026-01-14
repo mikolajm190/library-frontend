c31c8f280f6e3d6726829cfe2f2fedb2491a80c2
mikolajm190 <mikolaj.mo158+github@gmail.com>
2026-01-14 14:53:57 +0100

    fix: invalidate books/users queries correctly

--- src/api/queryKeys.ts
+++ src/api/queryKeys.ts
@@ -2,19 +2,29 @@ import type { GetBooksParams } from './books.api'
 import type { GetUsersParams } from './users.api'
 
 export const queryKeys = {
-  books: (params?: GetBooksParams) => [
-    'books',
-    params?.page ?? null,
-    params?.size ?? null,
-    params?.sortBy ?? null,
-    params?.sortOrder ?? null,
-  ],
+  books: (params?: GetBooksParams) => {
+    if (!params) {
+      return ['books'] as const
+    }
+    return [
+      'books',
+      params.page ?? null,
+      params.size ?? null,
+      params.sortBy ?? null,
+      params.sortOrder ?? null,
+    ] as const
+  },
   loans: () => ['loans'],
-  users: (params?: GetUsersParams) => [
-    'users',
-    params?.page ?? null,
-    params?.size ?? null,
-    params?.sortBy ?? null,
-    params?.sortOrder ?? null,
-  ],
+  users: (params?: GetUsersParams) => {
+    if (!params) {
+      return ['users'] as const
+    }
+    return [
+      'users',
+      params.page ?? null,
+      params.size ?? null,
+      params.sortBy ?? null,
+      params.sortOrder ?? null,
+    ] as const
+  },
 } as const
